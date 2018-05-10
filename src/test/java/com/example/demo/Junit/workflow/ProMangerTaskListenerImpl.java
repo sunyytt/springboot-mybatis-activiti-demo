3ffbe3254e2ad9b3d8cdd37a5b17cb159831c835
‘sunyy’ <122706747@qq.com>
2018-05-10 18:17:17 +0800

    留存测试使用实现TaskListener的方法动态传

---
+++ src/test/java/com/example/demo/Junit/workflow/ProMangerTaskListenerImpl.java
@@ -0,0 +1,11 @@
+package com.example.demo.Junit.workflow;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+public class ProMangerTaskListenerImpl implements TaskListener {
+    @Override
+    public void notify(DelegateTask delegateTask) {
+
+    }
+}
---
+++ src/test/java/com/example/demo/Junit/workflow/UserIdTaskListenerImpl.java
@@ -0,0 +1,15 @@
+package com.example.demo.Junit.workflow;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+public class UserIdTaskListenerImpl implements TaskListener {
+    @Override
+    public void notify(DelegateTask delegateTask) {
+        //指定个人任务的办理人，也可以指定组任务的办理人
+        //个人任务：通过类查询数据库，将下一个任务的办理人查询获取
+        // 根据业务查询，比如查到了"郭襄"
+        //然后通过setAssignee()的方法指定任务的办理人
+        delegateTask.setAssignee("郭襄");
+    }
+}
